810f786048e7bcb478215ef3ffc3841d4c0c618a
Andrius <andrius@wikiled.com>
2018-01-10 13:35:43 +0000

    Moving starter code

--- src/YiScanner/Monitoring/ServiceStarter.cs
+++ src/YiScanner/Monitoring/ServiceStarter.cs
@@ -1,8 +1,10 @@
 ﻿using System.IO;
+using System.Reactive.Concurrency;
 using Newtonsoft.Json;
 using NLog;
 using Topshelf;
 using Wikiled.YiScanner.Client;
+using Wikiled.YiScanner.Client.Archive;
 using Wikiled.YiScanner.Client.Predicates;
 
 namespace Wikiled.YiScanner.Monitoring
@@ -22,20 +24,22 @@ namespace Wikiled.YiScanner.Monitoring
             MonitoringConfig config = JsonConvert.DeserializeObject<MonitoringConfig>(File.ReadAllText(Path.Combine(directory, "service.json")));
             var predicate = config.All ? new NullPredicate() : (IPredicate)new NewFilesPredicate();
             DestinationFactory factory = new DestinationFactory(ftpConfiguration, config, predicate);
-            HostFactory.Run(x =>
-            {
-                x.Service<MonitoringInstance>(s =>
+            HostFactory.Run(
+                x =>
                 {
-                    s.ConstructUsing(name => new MonitoringInstance(config, factory));
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
+                    x.Service<MonitoringInstance>(
+                        s =>
+                        {
+                            s.ConstructUsing(name => new MonitoringInstance(TaskPoolScheduler.Default, config, factory, new DeleteArchiving()));
+                            s.WhenStarted(tc => tc.Start());
+                            s.WhenStopped(tc => tc.Stop());
+                        });
 
-                x.RunAsLocalSystem();
-                x.SetDescription("Camera Monitoring Service");
-                x.SetDisplayName("YiScanner Service");
-                x.SetServiceName("YiScanner");
-            });
+                    x.RunAsLocalSystem();
+                    x.SetDescription("Camera Monitoring Service");
+                    x.SetDisplayName("YiScanner Service");
+                    x.SetServiceName("YiScanner");
+                });
         }
     }
 }
